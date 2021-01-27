8c9a639d34c5da688a87d3d96f63533ab55671f0
untitledmonk <untitledmonk@gmail.com>
2021-01-27 09:12:53 +0530

    style the videoitems

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+
 
 class App extends React.Component{
+    state = {videos: []};
+
     onTermSubmit = async (term) => {
         console.log(`searching ${term}`);
         const results = await youtube.get('/search', {
@@ -11,12 +15,14 @@ class App extends React.Component{
             }
         });
         console.log(results);
+        this.setState({videos: results.data.items});
     };
 
     render(){
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <VideoList videos={this.state.videos}/>
             </div>
         )
     }
