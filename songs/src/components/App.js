1a753e27b35b0f6ff0daabbba1428f821d634d13
ankityara <c054117@yara.com>
2021-03-10 13:37:36 +0530

    some work on the songs program

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const App = () => {
     return (
       <h1>App</h1>
---
+++ songs/src/reducers/index.js
@@ -0,0 +1,16 @@
+export const songsReducer = () => {
+    return [
+        {'title': 'No Scrubs', duration: '4:05'},
+        {'title': 'Macarena', duration: '2:30'},
+        {'title': 'All Star', duration: '3:15'},
+        {'title': 'Behne de', duration: '1:45'}
+    ];
+};
+
+
+export const selectedSongReducer = (selectedSong=null, action) => {
+    if (action.type === 'SONG_SELECTED'){
+        return action.payload;
+    }
+    return selectedSong;
+};
