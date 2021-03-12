2f83b6a2f39c217b55f331f47eff23643b4816a4
ankityara <c054117@yara.com>
2021-03-12 10:45:28 +0530

    More redux work on songs

---
+++ songs/src/components/SongList.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from "../actions";
+
+
+class SongList extends Component {
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button
+                            className="ui button primary"
+                            onClick={() => this.props.selectSong(song)}
+                        >
+                            Select
+                        </button>
+                    </div>
+
+                    <div className="content">{song.title}</div>
+                </div>
+
+            );
+        });
+    };
+    render(){
+        console.log(this.props.songs);
+        return (
+            <div>
+                <div className="ui divided list">{this.renderList()}</div>
+            </div>
+        );
+    };
+}
+
+// "state" would contain all data from the state.
+const mapStateToProps = (state) =>{
+    console.log(state);
+    // This will be as props in the SongList component class.
+    return {
+        songs: state.songs
+    };
+};
+
+export default connect(mapStateToProps, {selectSong: selectSong})(SongList);
--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,4 +1,7 @@
-export const songsReducer = () => {
+import { combineReducers } from "redux";
+
+
+const songsReducer = () => {
     return [
         {'title': 'No Scrubs', duration: '4:05'},
         {'title': 'Macarena', duration: '2:30'},
@@ -8,9 +11,15 @@ export const songsReducer = () => {
 };
 
 
-export const selectedSongReducer = (selectedSong=null, action) => {
+const selectedSongReducer = (selectedSong=null, action) => {
     if (action.type === 'SONG_SELECTED'){
         return action.payload;
     }
     return selectedSong;
 };
+
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
